4d808aebd2824a012403a16a6a45c22b09598669
Nada Zirari <zirarinada20@gmail.com>
2025-03-21 14:13:41 +0000

    modified routes of tickets

--- CustomerAPI/routes/api.php
+++ CustomerAPI/routes/api.php
@@ -1,8 +1,19 @@
 <?php
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets', TicketController::class);
+    Route::apiResource('tickets.responses', ResponseController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
