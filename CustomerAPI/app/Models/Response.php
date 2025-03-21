7a12c1abb447d0ff76c37e96d7b063d7430b6455
Nada Zirari <zirarinada20@gmail.com>
2025-03-21 14:52:34 +0000

    added the fillable in model response

--- CustomerAPI/app/Models/Response.php
+++ CustomerAPI/app/Models/Response.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Response extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'ticket_id',
+        'user_id',
+        'content',
+    ];
 }
