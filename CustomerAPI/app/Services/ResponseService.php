770e389e5db86974e8e285e3c6bee59c04111d94
Nada Zirari <zirarinada20@gmail.com>
2025-03-21 14:51:24 +0000

    fixed file Responseservice

--- CustomerAPI/app/Services/ResponseService.php
+++ CustomerAPI/app/Services/ResponseService.php
@@ -2,13 +2,56 @@
 namespace App\Services;
 
 use App\Models\Response;
+use Illuminate\Support\Facades\Auth;
 
 class ResponseService {
-    public function addResponse(array $data) {
+
+
+
+    public function getAllResponses()
+    {
+        return Response::with('ticket')->get();
+    }
+
+public function getResponseById($id)
+    {
+        return Response::with('ticket')->find($id);
+    }
+
+
+    public function createResponse(array $data)
+    {
+        $data['user_id'] = Auth::id();
         return Response::create($data);
     }
 
-    public function getResponsesForTicket($ticketId) {
-        return Response::where('ticket_id', $ticketId)->get();
+public function updateResponse($id, array $data)
+    {
+        $response = Response::find($id);
+        
+        if (!$response) {
+            return null;
+        }
+        
+        $response->update($data);
+        return $response;
     }
+
+    public function deleteResponse($id)
+    {
+        $response = Response::find($id);
+        
+        if (!$response) {
+            return false;
+        }
+        
+        return $response->delete();
+    }
+    // public function addResponse(array $data) {
+    //     return Response::create($data);
+    // }
+
+    // public function getResponsesForTicket($ticketId) {
+    //     return Response::where('ticket_id', $ticketId)->get();
+    // }
 }
