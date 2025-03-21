2a550a0dc1a5a9efbad874a8c3ce50a63dd63cf0
Nada Zirari <zirarinada20@gmail.com>
2025-03-21 12:40:09 +0000

    add functions store in controller ticket

--- CustomerAPI/app/Http/Controllers/TicketController.php
+++ CustomerAPI/app/Http/Controllers/TicketController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function store(Request $request, TicketService $ticketService)
+{
+    $validated = $request->validate([
+        'title' => 'required|string|max:255',
+        'description' => 'required|string',
+    ]);
+
+    $ticket = $ticketService->createTicket([
+        'user_id' => auth()->id(),
+        'title' => $validated['title'],
+        'description' => $validated['description'],
+    ]);
+
+    return response()->json($ticket, 201);
+}
 }
