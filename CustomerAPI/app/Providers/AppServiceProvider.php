3c7c40bd9faf1c7bf780fcc336f727bd9281c4b7
Nada Zirari <zirarinada20@gmail.com>
2025-03-27 14:49:49 +0000

    added the necessary functions in the main controller

--- CustomerAPI/app/Providers/AppServiceProvider.php
+++ CustomerAPI/app/Providers/AppServiceProvider.php
@@ -1,7 +1,10 @@
+php artisan make:provider AppServiceProvider
 <?php
 
 namespace App\Providers;
 
+use App\Repositories\UserRepository;
+use App\Services\AuthService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,8 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ResponseService::class, function ($app) {
-            return new ResponseService();
+        $this->app->bind(UserRepository::class, function ($app) {
+            return new UserRepository($app->make('App\Models\User'));
+        });
+
+        $this->app->bind(AuthService::class, function ($app) {
+            return new AuthService($app->make(UserRepository::class));
         });
     }
 
